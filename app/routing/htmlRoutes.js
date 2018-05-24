21ecda6bd60a5a7297c21157f5f96b0cf14bfb04
jaethao <mjathao@gmail.com>
2018-05-24 14:18:50 -0500

    edits

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,7 @@
 var path = require("path");
 
 module.exports = function (app) {
-  // app.get("/survey", function(req, res) {
-  //   res.sendFile(path.join(__dirname + "/../public/survey.html"));
-  // });
-
-  // This will route the user to the home page
+	// This will route the user to the home page
 	app.use( function(req, res) {
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
